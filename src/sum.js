fac50c9989b99704f42d150bb64ec89325070e62
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-02-25 22:34:50 +0100

    Refactor sort code

--- src/sum.js
+++ src/sum.js
@@ -1,12 +1,12 @@
-function sum_numbers(number1, number2) {
-    return number1 + number2
-}
-
 function sum_1(number) {
     return number + 1
 }
 
+function sum_numbers(number1, number2) {
+    return number1 + number2
+}
+
 export const exportedForTesting = {
-    sum_numbers,
     sum_1
+    sum_numbers,
 }
--- test/sum.test.js
+++ test/sum.test.js
@@ -1,14 +1,14 @@
 import { exportedForTesting } from "../src/sum.js";
 
 
-describe("Check function sum_numbers", function() {
-    it('Check expected result', function() {
-        expect(exportedForTesting.sum_numbers(1,2)).toBe(3);
-    });
-});
-
 describe("Check function sum_1", function() {
     it('Check expected result', function() {
         expect(sum_1(1)).toBe(2);
+    });
+});
+
+describe("Check function sum_numbers", function() {
+    it('Check expected result', function() {
+        expect(exportedForTesting.sum_numbers(1,2)).toBe(3);
     });
 });
